4d2810814a43e02c9fc80c90ac8919ae4be6277e
wlkbfather <80864433+wlkbfather@users.noreply.github.com>
2021-03-29 17:16:28 +0800

    Update SimpleCtrl.java

--- src/main/java/tw/bill/ms/controller/SimpleCtrl.java
+++ src/main/java/tw/bill/ms/controller/SimpleCtrl.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api")
 public class SimpleCtrl {
 
-	@Value("${acoount}")
+	@Value("${account}")
 	private String account;
 	
 	@Value("${password}")
