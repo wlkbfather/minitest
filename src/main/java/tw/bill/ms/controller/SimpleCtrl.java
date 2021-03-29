d3a6adc4826d36e7303ca1f5238ede76ea45f705
wlkbfather <80864433+wlkbfather@users.noreply.github.com>
2021-03-29 10:03:36 +0800

    Update SimpleCtrl.java

--- src/main/java/tw/bill/ms/controller/SimpleCtrl.java
+++ src/main/java/tw/bill/ms/controller/SimpleCtrl.java
@@ -1,20 +1,32 @@
-package tw.bill.ms.controller;
+package com.example.demo.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api")
-public class SimpleCtrl {
-
+public class controller {
+	
+	@Value("${test01}")
+	private String test01;
+	
 	@GetMapping("/test01")
-	public String test01() {
+	public String example01() {
+		System.out.println("test01");
 		return "Hello World!!";
 	}
 	
 	@GetMapping("/test02")
-	public String test02() {
-		return "GOGOGO!!";
+	public String example02() {
+		System.out.println("test02");
+		return "GOGOGO!!!!";
 	}
+	@GetMapping("/test03")
+	public String example03() {
+		System.out.println("test03 = "+test01);
+		return "GOGOGO!!!!";
+	}
+	
 }
